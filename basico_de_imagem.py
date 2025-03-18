1e4e584055cb1cee2ad5575b1603eca0d495cd22
manetti19 <manetti.biazon@ime.eb.br>
2025-03-18 12:21:46 -0300

    asd

--- basico_de_imagem.py
+++ basico_de_imagem.py
@@ -1,9 +1,19 @@
-import cv2
+'''
+import cv2 as cv
 import matplotlib.pyplot as plt
 import numpy as np
 
-img_PID_2025 = cv2.imread("C:/Users/dinos/OneDrive/Imagens/Capturas de tela/Captura de tela 2025-03-12 114819.png", cv2.IMREAD_GRAYSCALE)
+img_PID_2025R = cv.imread('./Captura de tela 2025-03-12 114819.png')
 
-img_PID_2025R = cv2.imread("C:/Users/dinos/OneDrive/Imagens/Capturas de tela/Captura de tela 2025-03-12 114819.png")
+plt.imshow(img_PID_2025R, cmap='gray', vmin=0, vmax=255)
+plt.show()
+'''
+from PIL import Image  # Importa a biblioteca Pillow para abrir imagens
+import matplotlib.pyplot as plt  # Para exibir a imagem
 
-plt.imshow(img_PID_2025, cmap='gray', vmin=0, vmax=255)
+# Abre a imagem usando Pillow
+img_PID_2025R = Image.open('./Captura de tela 2025-03-12 114819.png')
+
+# Exibe a imagem usando matplotlib
+plt.imshow(img_PID_2025R, cmap='gray', vmin=0, vmax=255)
+plt.show()
